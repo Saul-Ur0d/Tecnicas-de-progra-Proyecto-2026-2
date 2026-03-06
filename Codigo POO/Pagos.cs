a27a7e9a02bcd9358e6f8e008022759c6cccc187
Osiel-421 <osielzc4@gmail.com>
2026-03-06 10:00:42 -0600

    pagos

--- Codigo POO/Pagos.cs
+++ Codigo POO/Pagos.cs
@@ -3,17 +3,73 @@
 
 bool continua = true;
 
-List<Ipago> list = new List<Ipago>();
+List<Ipago> listaPagos = new List<Ipago>();
 do
 {
     Console.WriteLine("Ingresa el monto a pagar : ");
-    
-    
-        string montotexto = Console.ReadLine() ?? "";
-        if ()
-    
+    string montotexto = Console.ReadLine() ?? "";
+    if (double.TryParse(montotexto, out double monto))
+    {
+        string modoPagoT;
+        int modoPago;
+        do
+        {
 
-}while (continua);
+            Console.WriteLine("1- Pago con tarjeta");
+            Console.WriteLine("2- Pago en efectivo");
+
+            modoPagoT = Console.ReadLine() ?? "";
+
+
+
+
+        } while (!int.TryParse(modoPagoT, out modoPago) || (modoPago != 1 && modoPago != 2));
+
+        if(modoPago == 1)
+        {
+            Console.WriteLine("Ingrese el numero de la tarjeta");
+            string tarjeta = Console.ReadLine() ?? "";
+
+            //Creando objeto para pago con tarjeta
+
+            Ipago pago = new PagoTarjeta(tarjeta, monto);
+            listaPagos.Add(pago);
+
+        }
+        else
+        {
+            Ipago pago = new PagoEfectivo(monto);
+            listaPagos.Add(pago);
+        }
+
+    }
+    else
+    {
+        Console.WriteLine("Error monto invalido");
+        return;
+    }
+
+    Console.WriteLine("Presiona S para procesar mas pagos: ");
+    char continuaT = char.Parse(Console.ReadLine() ?? "".ToLower());
+    if(continuaT == 's')
+    {
+        continua = true;
+    }
+    else
+    {
+        continua = false;
+    }
+
+
+} while (continua);
+
+foreach(Ipago pago in listaPagos)
+{
+    pago.ProcesarPago();
+}
+
+
+
 
 //Interfaz y clases
 
