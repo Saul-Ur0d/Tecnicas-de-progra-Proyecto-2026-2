29320a05b1ea275dfb858a572cbfb5efef98888f
Saul-Ur0d <saulurio06@gmail.com>
2026-03-18 10:03:31 -0600

    wAos

--- BibliotecaDeLibros/Class1.cs
+++ BibliotecaDeLibros/Class1.cs
@@ -1,7 +1,68 @@
-﻿namespace BibliotecaDeLibros
+﻿using BibliotecaDeLibros;
+
+namespace BibliotecaDeLibros
 {
-    public class Class1
+    public class Libro
     {
+        public string Titulo { get; set; }
+        public string Autor {  get; set; }
+        public int AnioPublicacion { get; set; }
+
+        public Libro (string titulo, string autor, int anioPublicacion)
+        {
+            Titulo = titulo;
+            Autor = autor;
+            AnioPublicacion = anioPublicacion;
+        }
+
+       
+        
+
+
+
+
+    }
+    public class GestorLibros
+    {
+        private List<Libro> libros = new List<Libro>();
+
+        public void AgregarLibro(Libro libro)
+        {
+            libros.Add(libro);
+        }
+
+        public void EliminarLibro(string titulo)
+        {
+            libros.RemoveAll(l => l.Titulo == titulo);
+        }
+
+        public List<Libro> BuscarLibrosPorAutor(string autor)
+        {
+            return libros.FindAll(l => l.Autor == autor);
+        }
+
+        public void MostrarLibros()
+        {
+            foreach (Libro libro in libros)
+            {
+                Console.WriteLine($"Titulo: {libro.Titulo}| Autor: {libro.Autor}| Año: {libro.AnioPublicacion}");
+            }
+        }
+
 
     }
 }
+
+/*
+namespace BiBliotek //guarda un espacio de trabajo donde puedo tener una clase con un nombre igual al de otra clase de otro espacio
+{
+    public class Calculadora { }
+    public class Calculadora { }
+}
+
+namespace BibBBB
+{
+    public class Calculadora { }
+}*/
+
+
---
+++ Codigo POO/Uso biblioteca.cs
@@ -0,0 +1,15 @@
+﻿
+using BibliotecaDeLibros;
+
+GestorLibros gestor = new GestorLibros();
+
+//Agregar Libro 
+
+gestor.AgregarLibro(new Libro("El señor de los anillos", "Tolkien", 1954));
+gestor.AgregarLibro(new Libro("Duna", "Frank", 1965));
+gestor.AgregarLibro(new Libro("Juego de tronos", "George", 1996));
+
+//Mostar libros
+
+Console.WriteLine("Libros actuales: ");
+gestor.MostrarLibros();
